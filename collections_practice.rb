1a8929c9afdff818cc6c767e82cb64d320fd49af
David Little <littledavid175@gmail.com>
2019-07-20 16:54:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+   if  a.length > b.length
+    +1
+    elsif a.length < b.length
+    -1
+   end
+  end
+end
+
+def swap_elements(array)
+  
+end
